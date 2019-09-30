c186f305535955801070e737246ad5557cdaa355
Avani Kala <avanikala21@gmail.com>
2019-09-30 14:23:15 -0700

    Added Book.java

---
+++ Book.java
@@ -0,0 +1,18 @@
+class Book{
+private int bookId;
+private String name;
+
+
+public void setbookId(){
+this.setbookId = bookId;
+}
+public int getbookId(){
+	return bookId;
+}
+public void setName(){
+this.setName = name;
+}
+public int getName(){
+	return name;
+}
+}
